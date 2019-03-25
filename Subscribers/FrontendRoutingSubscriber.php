3bb228f9767a8902abde68ff5aeac410cd50ed5f
Jakub Kułaga <kuba.kulaga.sv7@gmail.com>
2019-03-25 22:22:16 +0100

    UNDONE just started

--- Subscribers/FrontendRoutingSubscriber.php
+++ Subscribers/FrontendRoutingSubscriber.php
@@ -6,11 +6,28 @@ use Enlight\Event\SubscriberInterface;
 
 class FrontendRoutingSubscriber implements SubscriberInterface
 {
+    private $config;
+
+    public function __construct($config)
+    {
+        //todo $config mimo że podaje serwis klasy CachedConfigReader jest tablicą a nie obiektem CachedConfigReader, dlaczego?
+        $this->config = $config;
+    }
+
+
     public static function getSubscribedEvents()
     {
         return [
           'Enlight_Controller_Action_PreDispatch' => 'addTemplateDir',
+          'Enlight_Controller_Action_PostDispatchSecure_Frontend_Detail' => 'asignDetails',
         ];
+    }
+
+    public function asignDetails(\Enlight_Controller_ActionEventArgs $args)
+    {
+        /** @var \Shopware_Controllers_Frontend_Detail $subject */
+        $view = $args->getSubject()->View();
+        $view->assign('vDisplayFeaturedProducts', $this->config['display']);
     }
 
     public function addTemplateDir(\Enlight_Controller_ActionEventArgs $args)
